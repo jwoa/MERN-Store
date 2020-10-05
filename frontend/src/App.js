2d46965fc11c6251aebff3eb987cbd49b4b4a217
Jason Agyekum <jason@Jasons-MBP.home>
2020-10-05 15:59:54 -0400

    Added products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to S6</h1>
-    </>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/product/:id' component={ProductPage} />
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
